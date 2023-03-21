b19098cc8322bd4fcc84970b2e205b20dc0a48b6
hargrove-lab <103124833+hargrove-lab@users.noreply.github.com>
2023-03-21 13:25:41 +0800

    Update williams_plot.R

--- code_used_in_the_manuscript/williams_plot.R
+++ code_used_in_the_manuscript/williams_plot.R
@@ -17,9 +17,6 @@ testid <- id[-trainid]
 # assign testset and trainingset
 trainingset <- data[trainid,]
 testset <- data[-trainid,]
-
-
-
 x_train <- as.matrix(trainingset[-1])
 y_train <- data.matrix(trainingset[1])
 x <- x_train
@@ -34,8 +31,6 @@ mdl <- lm(formula = "lnKD~1+PEOE_VSA_POS+vsurf_DW12+vsa_other+vsurf_ID3",
 summary(mdl)
 
 # williams plot for lnKD
-library(matlib)
-library(ggplot2)
 # X_train <- cbind(trainingset$PEOE_VSA_POS,trainingset$vsurf_DW12,trainingset$vsa_other,trainingset$vsurf_ID3)
 # X_test <- cbind(testset$PEOE_VSA_POS,testset$vsurf_DW12,testset$vsa_other,testset$vsurf_ID3)
 wp_x <- cbind(data$PEOE_VSA_POS,data$vsurf_DW12,data$vsa_other,data$vsurf_ID3)
@@ -58,37 +53,5 @@ wp_mt[testid,3] <- stdres_test
 
 colnames(wp_mt)=c("id","hatvalue","stdres")
 
-ggplot(as.data.frame(wp_mt), aes(x=hatvalue,y=stdres))+
-  ggtitle(expression("Williams plot: lnK"[D]*"")) +
-  xlab(expression("Leverage")) + ylab(expression("Standardized residuals"))+
-  # THE DATA POINT
-  geom_point(aes(color = factor(id)),size = 5,alpha =1) +
-  xlim(0,0.8)+
-  ylim(-4,4)+
-  scale_color_manual(labels = c("Training set", "Test set"), values = c("dodgerblue", "red2"))+
-  
-  # title
-  theme_bw()+
-  theme(axis.ticks.length=unit(.4,"lines"))+
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())+
-  theme(axis.text.y = element_text(size = 20), 
-        axis.text.x = element_text(size=20), 
-        axis.title = element_text(size = 25,face = 'bold'),title =element_text(size = 25,face = 'bold') )+
-  # legend  
-  theme(legend.title = element_blank())+
-  theme(legend.text = element_text(colour="black", size=20, face="bold"))+
-  theme(legend.position = c(0.80, 0.1))+
-  # rec
-  theme(panel.background = element_rect(colour = "black", size = 3.5))+
-  # ref line  
-  geom_abline(intercept = 3, slope = 0, color="black", 
-              linetype="dashed", size=1.5)+
-  geom_abline(intercept = -3, slope = 0, color="black", 
-              linetype="dashed", size=1.5)+
-  geom_vline(xintercept = 3*5/36, color="black", 
-              linetype="dashed", size=1.5)
-
-  
-  plot(diag(h),stdred,col=c("blue4"),pch =19,cex = 2,cex.lab=2,cex.axis = 2,xlim=c(0,0.5),ylim=c(-4,4))
+plot(diag(h),stdred,col=c("blue4"),pch =19,cex = 2,cex.lab=2,cex.axis = 2,xlim=c(0,0.5),ylim=c(-4,4))
   
